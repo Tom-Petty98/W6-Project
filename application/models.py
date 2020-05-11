27572d3f0eb1a88aba1f1d13a8eb086a638e64b1
Tom Petty <tompetty527@yahoo.co.uk>
2020-05-11 21:19:46 +0100

    basic app structure

---
+++ application/models.py
@@ -0,0 +1,1 @@
+# no databse yet
---
+++ application/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from application import app
+# from application.models import ......
+
+@app.route('/')
+@app.route('/home')
+def home():
+#    postData = Posts.query.first()
+    return render_template('home.html', title="Home Page")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Page")
+
