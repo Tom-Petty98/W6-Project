f71725e994746bd43e86dfec830b59bbd102b4ea
Tom <tomp@gmail.com>
2020-05-24 19:59:44 +0000

    jenkins systemd setup

---
+++ Script/installation.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+ 
+sudo apt update -y
+ 
+sudo apt install python3 -y
+ 
+sudo apt install python3-pip -y
+ 
+sudo apt install python3-venv -y
+ 
+python3 -m venv project1-venv
+ 
+source ~/.bashrc
+ 
+source /var/lib/jenkins/workspace/Veggie_meals/project1-venv/bin/activate
+ 
+cd /var/lib/jenkins/workspace/Veggie_meals
+ 
+pip3 install -r requirements.txt
+ 
+pytest --cov ./application
+ 
+gunicorn --workers=4 application:app
